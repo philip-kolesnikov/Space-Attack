3103087161600c067160500b524ef6b4d259519d
Philip Kolesnikov <philip.kolesnikov.14@gmail.com>
2025-07-06 22:09:43 -0700

    Initial commit

---
+++ Assets/DragAndDrop.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragAndDrop : MonoBehaviour
+{
+    private Camera cam;
+    private Vector3 dragOffset;
+    public bool draggable;
+
+    public float followSpeed;
+
+    private void Awake()
+    {
+
+        cam = Camera.main;
+    }
+
+    private void OnMouseUp()
+    {
+        //drop the object
+        Drop();
+    }
+
+    //function that controls when the mouse button is down
+    private void OnMouseDown()
+    {
+        dragOffset = transform.position - getMousePos();
+    }
+
+    //fucntion that controls when the mouse is held down (dragging)
+    private void OnMouseDrag()
+    {
+        //if we can drag object
+        if (draggable)
+        {
+            //set the posistion of the object to the position of the mouse
+            //transform.position = getMousePos() + dragOffset;
+            transform.position = Vector3.MoveTowards(transform.position, getMousePos() + dragOffset, followSpeed * Time.deltaTime);
+        }
+        //set the position of object to mouses position
+    }
+
+    //function that gets the mouse position
+    private Vector3 getMousePos()
+    {
+        Vector3 mousePosition = cam.ScreenToWorldPoint(Input.mousePosition);
+        mousePosition.z = 0;
+        return mousePosition;
+    }
+
+    //function that controls what happens when we drop the game object
+    public void Drop()
+    {
+        if (draggable)
+        {
+            draggable = false;
+        }
+    }
+
+}
